f93b312456074ca43cda6ba558514651fb9fc35a
yazdabr <yzdgbt@gmail.com>
2026-09-22 10:40:08 +0800

    update admin

--- resources/views/components/admin/transaction-row.blade.php
+++ resources/views/components/admin/transaction-row.blade.php
@@ -171,7 +171,7 @@
                     class="flex w-full items-center gap-3 px-4 py-2.5 text-left text-sm font-medium text-red-600 transition hover:bg-red-50"
                 >
                     <x-heroicon-o-trash class="h-4 w-4 shrink-0"/>
-                    <span>Hapus</span>
+                    <span>Batalkan Transaksi?</span>
                 </button>
             @endif
         </div>
